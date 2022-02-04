a3cda63bcdda73b89ef39fd59e9c41ac20f0b88a
77Marty56 <96536243+77Marty56@users.noreply.github.com>
2022-02-04 11:57:59 +0100

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,66 @@
+### Assignment 1 - Exploratory Data Analysis Coursera 
+##### PLOT 4 #####
+
+### Load libraries
+library(lubridate)
+library(dplyr)
+
+### Change working directory
+#setwd("/Users/Elena/Desktop/EDA/Week1/Assignment1/Data")
+
+### Download the dataset
+filename <- "data1.zip"
+# Check if archive already exists
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileURL, filename)
+}
+# Check if folder exists
+if (!file.exists("household_power_consumption.txt")){
+  unzip(filename)
+}
+
+### Load the data into R
+household <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, 
+                        stringsAsFactors = FALSE, na.strings = "?", nrows=2075259, 
+                        check.names = FALSE, comment.char = "")
+# Turn Date and Time variable into Date object
+dateTime <- as_datetime(paste(household$Date, household$Time), 
+                        format = "%d/%m/%Y %H:%M:%S")
+
+household$dateTime <- dateTime
+household <- household %>% select(-c("Date", "Time"))
+
+
+### Making the plot
+# Create folder to store plot in
+if (!file.exists("plots")){
+  dir.create("plots")
+}
+
+# Subset the data to include only Feb 2007 day 1 and 2
+data <- household %>% filter(year(dateTime) == 2007 & month(dateTime) == 2 &
+                               (mday(dateTime) == 1 | mday(dateTime) == 2))
+
+# Make the plot
+png("./plots/plot4.png", width = 480, height = 480, units = "px")
+
+par(mfcol = c(2, 2))
+plot(data$dateTime, data$Global_active_power, col = "black", type = "l",
+     xlab = "", ylab = "Global Active Power")
+
+plot(data$dateTime, data$Sub_metering_1, type = "n", xlab = "", 
+     ylab = "Energy sub metering")
+lines(data$dateTime, data$Sub_metering_1, col = "black")
+lines(data$dateTime, data$Sub_metering_2, col = "red")
+lines(data$dateTime, data$Sub_metering_3, col = "blue")
+legend("topright", col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1))
+
+plot(data$dateTime, data$Voltage, xlab = "datetime", ylab = "Voltage", col = "black", 
+     type = "l")
+
+plot(data$dateTime, data$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power",
+     col = "black", type = "l")
+
+dev.off()
